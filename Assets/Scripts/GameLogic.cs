76e4d2a1b3b5069aaf86e73fd41445a7af78e1cc
JuneHong <jh4402@nyu.edu>
2016-09-19 20:51:42 -0400

    Committing hw assignment 2.

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+public class GameLogic : MonoBehaviour {
+
+	bool didPlayerwin = false; 
+	public Text Line; 
+	public Text Line2; 
+	public Transform Player; 
+	public Transform Treasure; 
+	public Transform Ball; 
+	public Transform Meat; 
+	public Transform Juice; 
+	public Transform Note; 
+
+
+	// Use this for initialization
+	void Start () {
+
+		Line.text = "You are a hobo (pan on your head) lost at your local swimming pool.";
+		Line2.text = "You need food. You can smell a scent coming directly south..."; 
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (didPlayerwin) {
+			Line.text = "You got a SOUP CAN!"; 
+			Line2.text = "Hopefully you can live another day..."; 
+
+			//SOUP CAN 
+		
+
+		} else if ((Player.position - Treasure.position).magnitude < 5f) {
+			Line.text = "Press SPACE to get soup can!";
+			if (Input.GetKeyDown (KeyCode.Space)) { 
+				didPlayerwin = true;
+			}
+
+		} else if ((Player.position - Ball.position).magnitude < 5f) { //BALL OBJECT 
+			Line.text = "Oh no! It's just a beach ball coated in olive oil.";
+			Line2.text = "But your nose senses something straight east..";
+
+		} else if ((Player.position - Meat.position).magnitude < 5f) { //MEAT OBJECT
+			Line.text = "It's someone's corndog, but you're a vegeterian."; 
+			Line2.text = "You pick up another scent faraway, north of the brown raft...";
+				
+		} else if ((Player.position - Juice.position).magnitude < 5f) { //TUBE OBJECT
+			Line.text = "It's a kid's empty juicebox.";
+			Line2.text = "You spy something to the west..."; 
+		} else if ((Player.position - Note.position).magnitude < 5f) { //NOTE OBJECT
+			Line.text = "A fellow pool hobo has left a note for you!";
+			Line2.text = "Head due south, young'un! Past the undersea hobo surveillance sphere!";
+		} 
+
+		 
+	} //void update
+} //class
+
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//move forward if I press W
+		if (Input.GetKey (KeyCode.W)) { //forward
+
+			transform.Translate (0f, 0f, 10f * Time.deltaTime); //translate always converts to local space
+
+			//transform.position += new Vector3 (0f, 0f, 1f); //world space movement
+
+			Camera.main.transform.position = transform.position + new Vector3 (-.16f, 6.03f, -7.34f); 
+ 
+		}
+
+		if (Input.GetKey (KeyCode.S)) { //backward
+
+			transform.Translate (0f, 0f, -10f * Time.deltaTime); //translate always converts to local space
+
+			//transform.position += new Vector3 (0f, 0f, 1f); //world space movement
+
+			Camera.main.transform.position = transform.position + new Vector3 (-.16f, 6.03f, -7.34f); 
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			transform.eulerAngles += new Vector3 (0f, -150f * Time.deltaTime, 0f); 
+			//transform.rotation *= Quaternion.Euler(0f, -5f, 0f); another way to rotate using Quaternion math
+			//transform.Rotate(0f,-5f,0f); //more intuitive way to turn left? 
+
+			Camera.main.transform.position = transform.position + new Vector3 (-.16f, 6.03f, -7.34f); 
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			transform.eulerAngles += new Vector3 (0f, 150f * Time.deltaTime, 0f); 
+			//transform.rotation *= Quaternion.Euler(0f, -5f, 0f); another way to rotate using Quaternion math
+			//transform.Rotate(0f,-5f,0f); //more intuitive way to turn left? 
+
+			Camera.main.transform.position = transform.position + new Vector3 (-.16f, 6.03f, -7.34f);  
+
+		}
+	}
+}
